def167bc13269a22133f65d2496e722475937dc7
Tomislav Dukez <tomdu3@ymail.com>
2023-04-26 17:49:36 +0000

    map

--- map.js
+++ map.js
@@ -5,16 +5,27 @@
  */
 
 // Using a for loop
-
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results)
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults)
 
 // Simplified w/ map()
+console.log(nums.map(function(num) { return num * 2}))
 
 
 // Simplfied w/ map() + arrow function
 
+console.log(nums.map(num => num * 2))
 
 // With objects:
 const students = [
@@ -37,3 +48,14 @@ const students = [
     skill: 'CSS'
   },
 ];
+let age = {
+  Mark:32,
+  Ariel: 26,
+  Jason: 28}
+
+console.log(students.map(student => [student.name, student.id]));
+console.log(students.map(student => ({name:student.name, id:student.id})));
+console.log(students.map(student => {
+  student.age = age[`${student.name}`]
+  return student;
+}))
